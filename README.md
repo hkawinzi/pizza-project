f85f7731a9c626592905d94ff7474d0610df7669
happiness <happinesskawinzi@gmail.com>
2019-07-31 05:11:42 +0300

    correct indentation

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 ## project name
-pizza
+pizza-project
 
-## desc
+## description
+this is an application that allows one to place an order,be alerted the price and also has options which one can choose from.
+
+#### date-month-year
+30-07-2019
+
+#### Bugs
+The web functionality is best in an internet connected device.
+
+#### installation
+*open browser and open Github
+*search hkawinzi and chose pizza-project
+*clone the project
+
+#### technologies used 
+HTML
+Css
+JQuery
+bootstrap
+
+#### license
+MIT License
+
+Copyright (c) [2019] [Happiness Kawinzi]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+#### by
+Happiness Kawinzi.if you have any queries you can email me "happinesskawinzi@gmail.com" 
--- js/pizza.js
+++ js/pizza.js
@@ -1,128 +1,130 @@
-$(function(){
-  $(".onclick").click(function(){
-     $("#hidden1").slideToggle(200);
-   });
-  
-   $(".onclick1").click(function(){
-     $("#hidden2").slideToggle(200);
-   });
+$(function () {
+  $(".onclick").click(function () {
+    $("#hidden1").slideToggle(200);
+  });
 
-   $(".onclick2").click(function(){
-     $("#hidden3").slideToggle(200);
-   });
+  $(".onclick1").click(function () {
+    $("#hidden2").slideToggle(200);
+  });
 
-   $(".onclick3").click(function(){
+  $(".onclick2").click(function () {
+    $("#hidden3").slideToggle(200);
+  });
+
+  $(".onclick3").click(function () {
     $("#hidden4").slideToggle(200);
   });
 
-  $(".onclick4").click(function(){
+  $(".onclick4").click(function () {
     $("#hidden5").slideToggle(200);
   });
 
-  $(".onclick5").click(function(){
+  $(".onclick5").click(function () {
     $("#hidden6").slideToggle(200);
   });
-   
-    
-    var price , crust_price, topping_price ;
-    function Getpizza( name,size,crust,topping, total ){
-      this.name = name;
-      this.size = size;
-      this.crust = crust;
-      this.topping = topping;
-      this.total = total;
-    }
 
-  $("button.proceed").click(function(){
+
+  var price, crust_price, topping_price;
+  function Getpizza(name, size, crust, topping, total) {
+    this.name = name;
+    this.quantity = quantity;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.total = total;
+  }
+
+  $("button.proceed").click(function () {
     $("button.proceed").hide();
     $("div.choice").show();
   });
 
-  $("button.proceed").click(function(event){
-     let pname = $(".name option:selected").val();
-     let psize = $("#size option:selected").val();
-     let pcrust = $("#crust option:selected").val();
-     let ptopping = [];
-     $.each($("input[name='toppings']:checked"), function(){
-         ptopping.push($(this).val());
-     });
-     console.log(ptopping.join(", "));
+  $("button.proceed").click(function (event) {
+    let pname = $(".name option:selected").val();
+    let pquantity = $(".quantity option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
+    $.each($("input[name='toppings']:checked"), function () {
+      ptopping.push($(this).val());
     });
+    console.log(ptopping.join(", "));
+  });
 
-    $("button#final-order").click(function(){
-      let person = $("input#name").val();
-      let phone = $("input#phone").val();
-      let location = $("input#location").val();
-      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
-  
-        $("#delivery-message").append(person+" We have recieved your order and it will be delivered to "+location);
-      }
-      else {
-        alert("Please fill in the details for delivery!");
-      }
-    });
+  $("button#final-order").click(function () {
+    let person = $("input#name").val();
+    let phone = $("input#phone").val();
+    let location = $("input#location").val();
+    if ($("input#name").val() && $("input#phone").val() && $("input#location").val() != "") {
 
-    $("button.proceed").click(function(event){
-      let mname = $(".name option:selected").val();
-      let msize = $("#size option:selected").val();
-      let mtopping = $("#topping option:selected").val();
-   
-      $("#pizzaname").append("Pizza Name - "+mname);
-      $("#pizzasize").append("Pizza size - "+msize);
-      $("#pizzatopping").append("Pizza Toppings - "+mtopping);
-      
-      switch(msize){
-        case "mega":
-           price = 1200;
-           console.log(price);
-         break;
-       case "large":
-          price = 1000;
-          console.log(price);
-        break;
-        case "medium":
-          price = 800;
-          console.log(price);
-        break;
-        case "small":
-          price = 500;
-          console.log(price);
-        default:
-          console.log("error");
-      }
-   
-      switch(mtopping){
-       case "smoked-sausage":
-         topping_price = 100;
-       break;
-       case "vienna-sausage":
-         topping_price = 100;
-       break;
-       case "bacon":
-         topping_price = 80;
-       break;
-       case "cheese":
-         topping_price = 60;
-       break;
-       default:
-         console.log("No price");
+      $("#delivery-message").append(person + " We have recieved your order and it will be delivered to " + location);
     }
-   
-   
-      let mtotal = price + crust_price + topping_price;
-      console.log(mtotal);
-      $("#pizzatotal").append("Your Total is:  sh. "+mtotal);
-   
-        var newOrder = new Getpizza(mname, msize,mtopping,mtotal);
-     console.log(newOrder);
-     $(".name option:selected").val("");
-     $("#size option:selected").val("");
-     $("#crust option:selected").val("");
-     $("#topping option:selected").val("");
-   
-     
-      event.preventDefault();
-     
-     });
+    else {
+      alert("Please fill in the details for delivery!");
+    }
+  });
+
+  $("button.proceed").click(function (event) {
+    let mname = $(".name option:selected").val();
+    let msize = $("#size option:selected").val();
+    let mtopping = $("#topping option:selected").val();
+
+    $("#pizzaname").append("Pizza Name - " + mname);
+    $("#pizzasize").append("Pizza size - " + msize);
+    $("#pizzatopping").append("Pizza Toppings - " + mtopping);
+
+    switch (msize) {
+      case "mega":
+        price = 1200;
+        console.log(price);
+        break;
+      case "large":
+        price = 1000;
+        console.log(price);
+        break;
+      case "medium":
+        price = 800;
+        console.log(price);
+        break;
+      case "small":
+        price = 500;
+        console.log(price);
+      default:
+        console.log("error");
+    }
+
+    switch (mtopping) {
+      case "smoked-sausage":
+        topping_price = 100;
+        break;
+      case "vienna-sausage":
+        topping_price = 100;
+        break;
+      case "bacon":
+        topping_price = 80;
+        break;
+      case "cheese":
+        topping_price = 60;
+        break;
+      default:
+        console.log("No price");
+    }
+
+
+    let mtotal = price + crust_price + topping_price;
+    console.log(mtotal);
+    $("#pizzatotal").append("Your Total is:  sh. " + mtotal);
+
+    var newOrder = new Getpizza(mname, msize, mtopping, mtotal);
+    console.log(newOrder);
+    $(".name option:selected").val("");
+    $("#size option:selected").val("");
+    $("#crust option:selected").val("");
+    $("#topping option:selected").val("");
+
+
+    event.preventDefault();
+
+  });
 
 });
